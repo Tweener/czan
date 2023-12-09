94d9f5275006bbec7259bb6f55ec1075dbb4d5e0
Vivien Mahé <vivouneuh@gmail.com>
2023-12-09 12:22:04 +0100

    Adds more customization on GlideImage

--- library/src/main/java/com/tweener/czan/android/designsystem/atom/image/Image.kt
+++ library/src/main/java/com/tweener/czan/android/designsystem/atom/image/Image.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.components.imageComponent
 import com.skydoves.landscapist.glide.GlideImage
@@ -20,17 +22,24 @@ import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
 
 @Composable
 fun Image(
-    color: Color,
-    highlightColor: Color,
     modifier: Modifier = Modifier,
     imageUrl: String? = null,
+    color: Color = Color.Transparent,
+    highlightColor: Color = Color.Transparent,
     contentScale: ContentScale = ContentScale.Crop,
     @DrawableRes placeholder: Int = 0,
+    dimensions: Pair<Int, Int>? = null,
 ) {
     GlideImage(
         modifier = modifier,
         imageModel = { imageUrl },
         imageOptions = ImageOptions(contentScale = contentScale, alignment = Alignment.Center),
+        requestOptions = {
+            with(RequestOptions()) {
+                dimensions?.let { override(it.first, it.second) }
+                diskCacheStrategy(DiskCacheStrategy.ALL)
+            }
+        },
         previewPlaceholder = placeholder,
         component = imageComponent {
             +ShimmerPlugin(
