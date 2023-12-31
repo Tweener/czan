181b54a91bc4205ee30110b298014a42b29ca136
Vivien Mahé <vivouneuh@gmail.com>
2023-12-31 13:52:54 +0100

    Adds Jitpack to fetch libraries but not to publish

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
@@ -10,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://jitpack.io") }
     }
 }
 
