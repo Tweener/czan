25c66f3a16a537798cd51fdc4e38f97f41cfccb8
Vivien Mahé <vivouneuh@gmail.com>
2024-01-03 10:13:21 +0100

    Adds StateFlow collectAsStateMultiplatform extension

---
+++ czan/src/main/java/com/tweener/czan/preview/designsystem/atom/image/ImagePreview.kt
@@ -0,0 +1,24 @@
+package com.tweener.czan.preview.designsystem.atom.image
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.tweener.czan.preview.UiModePreviews
+import com.tweener.czan.designsystem.atom.image.Image
+import com.tweener.czan.preview.CzanThemePreview
+
+/**
+ * @author Vivien Mahe
+ * @since 01/01/2024
+ */
+
+@UiModePreviews
+@Composable
+private fun ImageFromUrlPreview() {
+    CzanThemePreview {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            imageUrl = "https://images.pexels.com/photos/2223082/pexels-photo-2223082.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        )
+    }
+}
