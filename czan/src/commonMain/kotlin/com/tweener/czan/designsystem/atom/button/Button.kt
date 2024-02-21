0ddd322ddc3925f89a5fa3d2a5bb64604ad8cb11
Vivien Mahé <vivouneuh@gmail.com>
2024-02-21 18:12:09 +0100

    CZAN Button should not be adapted to Cupertino look & feel

--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/button/Button.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/button/Button.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -13,15 +14,12 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.tweener.czan.designsystem.atom.text.Text
 import com.tweener.czan.theme.Size
-import io.github.alexzhirkevich.cupertino.adaptive.AdaptiveButton
-import io.github.alexzhirkevich.cupertino.adaptive.ExperimentalAdaptiveApi
 
 /**
  * @author Vivien Mahe
  * @since 24/08/2023
  */
 
-@OptIn(ExperimentalAdaptiveApi::class)
 @Composable
 fun Button(
     modifier: Modifier = Modifier,
@@ -36,23 +34,18 @@ fun Button(
 ) {
     val shouldUseIconPadding = (leadingIcon != null || trailingIcon != null) && text != null
 
-    AdaptiveButton(
+    Button(
         modifier = modifier.height(size.height),
         enabled = enabled,
         onClick = { onClick?.invoke() },
-        adaptation = {
-            material {
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = if (outlined) Color.Transparent else style.containerColor,
-                    contentColor = if (outlined) style.containerColor else style.contentColor,
-                    disabledContainerColor = style.disabledContainerColor,
-                    disabledContentColor = style.disabledContentColor
-                )
-                shape = size.shape
-                contentPadding = if (shouldUseIconPadding) size.contentWithIconPadding else size.contentPadding
-            }
-            cupertino { }
-        },
+        contentPadding = if (shouldUseIconPadding) size.contentWithIconPadding else size.contentPadding,
+        shape = size.shape,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (outlined) Color.Transparent else style.containerColor,
+            contentColor = if (outlined) style.containerColor else style.contentColor,
+            disabledContainerColor = style.disabledContainerColor,
+            disabledContentColor = style.disabledContentColor
+        ),
         border = if (outlined) BorderStroke(1.dp, style.containerColor) else null
     ) {
         if (leadingIcon != null) {
