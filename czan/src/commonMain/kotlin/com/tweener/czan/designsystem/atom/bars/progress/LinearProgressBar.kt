435164863ea0f9b5928b20a0967b716782ebb583
Vivien Mahé <vivouneuh@gmail.com>
2024-03-14 11:04:34 +0100

    Adds customization of strokeCap to LinearProgressBar

--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/bars/progress/LinearProgressBar.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/bars/progress/LinearProgressBar.kt
@@ -2,9 +2,11 @@ package com.tweener.czan.designsystem.atom.bars.progress
 
 import androidx.annotation.FloatRange
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import com.tweener.czan.theme.CzanUiDefaults
 
 /**
@@ -17,12 +19,14 @@ fun LinearProgressBar(
     modifier: Modifier = Modifier,
     @FloatRange(from = 0.0, to = 1.0) progress: Float = 0f,
     color: Color = CzanUiDefaults.ProgressBar.color,
-    selectedColor: Color = CzanUiDefaults.ProgressBar.selectedColor
+    selectedColor: Color = CzanUiDefaults.ProgressBar.selectedColor,
+    strokeCap: StrokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
 ) {
     LinearProgressIndicator(
         modifier = modifier,
         progress = { progress },
         color = color,
-        trackColor = selectedColor
+        trackColor = selectedColor,
+        strokeCap = strokeCap,
     )
 }
