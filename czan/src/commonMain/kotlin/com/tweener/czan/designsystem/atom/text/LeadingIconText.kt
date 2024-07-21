4204b6d509863f10550d03faac6e05d94fbe914c
Vivien Mahé <vivouneuh@gmail.com>
2024-07-21 16:13:21 +0200

    LeadingIconText can now use AnnotatedString

--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/text/LeadingIconText.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/text/LeadingIconText.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -19,14 +20,12 @@ import androidx.compose.ui.unit.dp
 import com.tweener.czan.designsystem.atom.icon.Icon
 import com.tweener.czan.theme.Size
 import org.jetbrains.compose.resources.DrawableResource
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 /**
  * @author Vivien Mahe
  * @since 20/04/2024
  */
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun LeadingIconText(
     text: String,
@@ -110,8 +109,82 @@ fun LeadingIconText(
 }
 
 @Composable
+fun LeadingIconText(
+    text: AnnotatedString,
+    icon: DrawableResource,
+    modifier: Modifier = Modifier,
+    contentPadding: Dp = Size.Padding.Small,
+    iconSize: Dp = 24.dp,
+    iconTint: Color = Color.Unspecified,
+    style: TextStyle = LocalTextStyle.current,
+    color: Color = Color.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    lines: Int? = null,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+) {
+    LeadingIconText(
+        modifier = modifier,
+        text = text,
+        icon = {
+            Icon(
+                modifier = Modifier.size(iconSize),
+                resource = icon,
+                tint = iconTint,
+            )
+        },
+        contentPadding = contentPadding,
+        style = style,
+        color = color,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        overflow = overflow,
+        lines = lines,
+        maxLines = maxLines,
+        minLines = minLines,
+    )
+}
+
+@Composable
 private fun LeadingIconText(
     text: String,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: Dp = Size.Padding.Small,
+    style: TextStyle = LocalTextStyle.current,
+    color: Color = Color.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    lines: Int? = null,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+) {
+    LeadingIconText(
+        modifier = modifier,
+        text = AnnotatedString(text),
+        icon = icon,
+        contentPadding = contentPadding,
+        style = style,
+        color = color,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        overflow = overflow,
+        lines = lines,
+        maxLines = maxLines,
+        minLines = minLines,
+    )
+}
+
+@Composable
+private fun LeadingIconText(
+    text: AnnotatedString,
     icon: @Composable () -> Unit,
     modifier: Modifier = Modifier,
     contentPadding: Dp = Size.Padding.Small,
