e825332781eb77da28a249f3a140eaad8040fff8
Vivien Mahé <vivouneuh@gmail.com>
2024-09-05 14:48:52 +0200

    LinearProgressBar - Progress can now be animated

--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/bars/progress/LinearProgressBar.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/bars/progress/LinearProgressBar.kt
@@ -1,9 +1,14 @@
 package com.tweener.czan.designsystem.atom.bars.progress
 
 import androidx.annotation.FloatRange
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
@@ -17,14 +22,32 @@ import com.tweener.czan.theme.CzanUiDefaults
 @Composable
 fun LinearProgressBar(
     modifier: Modifier = Modifier,
+    animated: Boolean = false,
+    animationDurationMillis: Int = 300,
     @FloatRange(from = 0.0, to = 1.0) progress: Float = 0f,
     color: Color = CzanUiDefaults.ProgressBar.selectedColor,
     trackColor: Color = CzanUiDefaults.ProgressBar.trackColor,
     strokeCap: StrokeCap = ProgressIndicatorDefaults.LinearStrokeCap,
 ) {
+    val progressAnimation = remember { Animatable(0f) }
+
+    LaunchedEffect(progress) {
+        if (animated) {
+            progressAnimation.animateTo(
+                targetValue = progress,
+                animationSpec = tween(
+                    durationMillis = (animationDurationMillis * (1f - progressAnimation.value)).toInt(),
+                    easing = LinearEasing,
+                ),
+            )
+        } else {
+            progressAnimation.snapTo(progress)
+        }
+    }
+
     LinearProgressIndicator(
         modifier = modifier,
-        progress = { progress },
+        progress = { progressAnimation.value },
         color = color,
         trackColor = trackColor,
         strokeCap = strokeCap,
