2e093297b2fef7f101aa548712f4dd80f50d535a
Vivien Mahé <vivouneuh@gmail.com>
2024-10-08 16:28:01 +0200

    Adds a Modifier.onHoldTouch() extension function to handle a tap event where the user keeps holding the Composable

--- czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/ModifierExtensions.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/ModifierExtensions.kt
@@ -1,27 +1,41 @@
 package com.tweener.czan._internal.kotlinextensions
 
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.FloatAnimationSpec
+import androidx.compose.animation.core.FloatTweenSpec
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import com.valentinilk.shimmer.ShimmerBounds
 import com.valentinilk.shimmer.defaultShimmerTheme
 import com.valentinilk.shimmer.rememberShimmer
 import com.valentinilk.shimmer.shimmer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * @author Vivien Mahe
@@ -137,4 +151,40 @@ fun Modifier.shimmer(
     )
 
     shimmer(customShimmer = shimmer)
+}
+
+fun Modifier.onHoldTouch(
+    easing: Easing = FastOutSlowInEasing,
+    pollDelay: Duration = 50.milliseconds,
+    targetPollDelay: Duration = pollDelay,
+    animationDuration: Duration = 3.seconds,
+    onTouchHeld: () -> Unit,
+) = composed {
+    val scope = rememberCoroutineScope()
+
+    pointerInput(Unit) {
+        val animationSpec: FloatAnimationSpec = FloatTweenSpec(duration = animationDuration.inWholeMilliseconds.toInt(), delay = 0, easing = easing)
+
+        awaitEachGesture {
+            val initialDown = awaitFirstDown(requireUnconsumed = false)
+            val initialTouchHeldJob = scope.launch {
+                var currentPlayTime = 0.milliseconds
+                var delay = pollDelay
+                while (initialDown.pressed) {
+                    onTouchHeld()
+
+                    delay(delay.inWholeMilliseconds)
+                    currentPlayTime += delay
+                    delay = animationSpec.getValueFromNanos(
+                        playTimeNanos = currentPlayTime.inWholeNanoseconds,
+                        initialValue = pollDelay.inWholeMilliseconds.toFloat(),
+                        targetValue = targetPollDelay.inWholeMilliseconds.toFloat(),
+                        initialVelocity = 0F,
+                    ).toInt().milliseconds
+                }
+            }
+            waitForUpOrCancellation()
+            initialTouchHeldJob.cancel()
+        }
+    }
 }
