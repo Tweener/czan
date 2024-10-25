d4bd92a51efaed46f20fe9572680fd262fcec02d
Vivien Mahé <vivouneuh@gmail.com>
2024-10-25 10:13:27 +0200

    Slider - Use SliderDefaults for customizing colors

--- czan/src/androidMain/kotlin/com/tweener/czan/preview/designsystem/atom/slider/SliderPreview.kt
+++ czan/src/androidMain/kotlin/com/tweener/czan/preview/designsystem/atom/slider/SliderPreview.kt
@@ -7,8 +7,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import com.tweener.czan.designsystem.atom.slider.Slider
-import com.tweener.czan.designsystem.atom.slider.SliderStyle
+import com.tweener.czan.designsystem.atom.slider.SliderDefaults
 import com.tweener.czan.preview.CzanThemePreview
 import com.tweener.czan.preview.UiModePreviews
 
@@ -16,8 +17,6 @@ import com.tweener.czan.preview.UiModePreviews
  * @author Vivien Mahe
  * @since 25/10/2024
  */
-
-// region Preview Primary
 
 @UiModePreviews
 @Composable
@@ -27,60 +26,17 @@ private fun SliderPrimaryCheckedPreview() {
     CzanThemePreview {
         Column {
             Slider(
-                value = 0f,
-                valueRange = 0f..10f,
-                onValueChange = { sliderPosition = it }
-            )
-
-            Text(text = "Slider value: $sliderPosition")
-        }
-    }
-}
-
-// endregion Preview Primary
-
-// region Preview Secondary
-
-@UiModePreviews
-@Composable
-private fun SliderSecondaryCheckedPreview() {
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
-
-    CzanThemePreview {
-        Column {
-            Slider(
-                style = SliderStyle.SECONDARY,
                 value = 4f,
                 valueRange = 0f..10f,
-                onValueChange = { sliderPosition = it }
+                onValueChange = { sliderPosition = it },
+                colors = SliderDefaults.sliderColors(
+                    thumbColor = Color.Blue,
+                    activeTrackColor = Color.Red,
+                    inactiveTrackColor = Color.Green,
+                ),
             )
 
             Text(text = "Slider value: $sliderPosition")
         }
     }
 }
-
-// endregion Preview Secondary
-
-// region Preview Tertiary
-
-@UiModePreviews
-@Composable
-private fun SliderTertiaryCheckedPreview() {
-    var sliderPosition by remember { mutableFloatStateOf(0f) }
-
-    CzanThemePreview {
-        Column {
-            Slider(
-                style = SliderStyle.TERTIARY,
-                value = 10f,
-                valueRange = 0f..10f,
-                onValueChange = { sliderPosition = it }
-            )
-
-            Text(text = "Slider value: $sliderPosition")
-        }
-    }
-}
-
-// endregion Preview Tertiary
--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/slider/Slider.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/slider/Slider.kt
@@ -5,9 +5,11 @@ package com.tweener.czan.designsystem.atom.slider
  * @since 25/10/2024
  */
 
-import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import io.github.alexzhirkevich.cupertino.adaptive.AdaptiveSlider
 import io.github.alexzhirkevich.cupertino.adaptive.ExperimentalAdaptiveApi
 
@@ -17,7 +19,7 @@ fun Slider(
     value: Float,
     onValueChange: (Float) -> Unit,
     modifier: Modifier = Modifier,
-    style: SliderStyle = SliderStyle.PRIMARY,
+    colors: SliderColors = SliderDefaults.sliderColors(),
     enabled: Boolean = true,
     valueRange: ClosedFloatingPointRange<Float> = 0f..1f,
     steps: Int = 0,
@@ -31,12 +33,42 @@ fun Slider(
         steps = steps,
         adaptation = {
             material {
-                colors = SliderDefaults.colors(
-                    thumbColor = style.thumbColor,
-                    activeTrackColor = style.activeTrackColor,
-                    inactiveTrackColor = style.inactiveTrackColor,
+                this.colors = androidx.compose.material3.SliderDefaults.colors(
+                    thumbColor = colors.thumbColor(),
+                    activeTrackColor = colors.activeTrackColor(),
+                    inactiveTrackColor = colors.inactiveTrackColor(),
                 )
             }
         },
     )
+}
+
+object SliderDefaults {
+
+    @Composable
+    fun sliderColors(
+        thumbColor: Color = MaterialTheme.colorScheme.primary,
+        activeTrackColor: Color = MaterialTheme.colorScheme.primary,
+        inactiveTrackColor: Color = MaterialTheme.colorScheme.outline,
+    ): SliderColors = SliderColors(
+        thumbColor = thumbColor,
+        activeTrackColor = activeTrackColor,
+        inactiveTrackColor = inactiveTrackColor,
+    )
+}
+
+@Immutable
+class SliderColors internal constructor(
+    private val thumbColor: Color,
+    private val activeTrackColor: Color,
+    private val inactiveTrackColor: Color,
+) {
+    @Composable
+    internal fun thumbColor(): Color = thumbColor
+
+    @Composable
+    internal fun activeTrackColor(): Color = activeTrackColor
+
+    @Composable
+    internal fun inactiveTrackColor(): Color = inactiveTrackColor
 }
