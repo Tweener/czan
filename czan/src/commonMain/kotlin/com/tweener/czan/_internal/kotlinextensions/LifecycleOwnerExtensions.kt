4b4b47510b890603bf1756ad5e1608c5386d7603
Vivien Mahé <vivouneuh@gmail.com>
2025-01-24 11:19:52 +0100

    Add LifecycleOwner.onLifecycleEvents to observe lifecycle events

--- czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/LifecycleOwnerExtensions.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/LifecycleOwnerExtensions.kt
@@ -8,11 +8,36 @@ import androidx.lifecycle.LifecycleOwner
 
 /**
  * @author Vivien Mahe
- * @since 26/08/2023
+ * @since 21/01/2025
  */
 
+/**
+ * Observes lifecycle events within a Composable and executes corresponding callbacks when events occur.
+ *
+ * This function provides a way to listen to lifecycle changes inside a Composable,
+ * similar to `LifecycleObserver`, and is useful for managing UI state and resources.
+ *
+ * ### Example Usage:
+ * ```
+ * @Composable
+ * fun MyScreen(lifecycleOwner: LifecycleOwner) {
+ *     lifecycleOwner.onLifecycleEvents(
+ *         onResume = { println("Screen resumed!") },
+ *         onPause = { println("Screen paused!") }
+ *     )
+ * }
+ * ```
+ *
+ * @param onCreate Callback invoked when the lifecycle event `ON_CREATE` occurs.
+ * @param onStart Callback invoked when the lifecycle event `ON_START` occurs.
+ * @param onResume Callback invoked when the lifecycle event `ON_RESUME` occurs.
+ * @param onPause Callback invoked when the lifecycle event `ON_PAUSE` occurs.
+ * @param onStop Callback invoked when the lifecycle event `ON_STOP` occurs.
+ * @param onDestroy Callback invoked when the lifecycle event `ON_DESTROY` occurs.
+ * @param onAny Callback invoked when any lifecycle event occurs.
+ */
 @Composable
-fun LifecycleOwner.onEventsForComposable(
+fun LifecycleOwner.onLifecycleEvents(
     onCreate: (() -> Unit)? = null,
     onStart: (() -> Unit)? = null,
     onResume: (() -> Unit)? = null,
