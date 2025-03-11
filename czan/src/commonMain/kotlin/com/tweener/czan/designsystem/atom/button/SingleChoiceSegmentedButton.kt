7c69e788555f82cd5ab8e29dfc4f6ee1487faab7
Vivien Mahé <vivouneuh@gmail.com>
2025-03-11 06:45:29 +0100

    SingleChoiceSegmentedButton - Use rememberSaveable for selected index

--- czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/button/SingleChoiceSegmentedButton.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/designsystem/atom/button/SingleChoiceSegmentedButton.kt
@@ -12,7 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +28,7 @@ fun SingleChoiceSegmentedButton(
     colors: SegmentedButtonColors = SegmentedButtonDefaults.colors(),
     onOptionSelected: (Int) -> Unit,
 ) {
-    var selectedIndex by remember { mutableIntStateOf(selectedOption.coerceIn(0, options.size - 1)) }
+    var selectedIndex by rememberSaveable { mutableIntStateOf(selectedOption.coerceIn(0, options.size - 1)) }
 
     SingleChoiceSegmentedButtonRow(modifier = modifier) {
         options.forEachIndexed { index, label ->
