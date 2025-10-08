5e143a3e014b926ec4a87781340dfbd9f52f3416
Vivien Mahé <vivouneuh@gmail.com>
2025-10-08 23:23:24 +0200

    Add dashedBorder modifier for custom borders
    Introduces a dashedBorder extension for Modifier, allowing customizable dashed borders with color, shape, stroke width, dash length, gap length, and stroke cap. This enhances UI flexibility for Compose components.

--- czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/ModifierExtensions.kt
+++ czan/src/commonMain/kotlin/com/tweener/czan/_internal/kotlinextensions/ModifierExtensions.kt
@@ -21,12 +21,20 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.valentinilk.shimmer.ShimmerBounds
 import com.valentinilk.shimmer.defaultShimmerTheme
 import com.valentinilk.shimmer.rememberShimmer
@@ -187,4 +195,42 @@ fun Modifier.onHoldTouch(
             initialTouchHeldJob.cancel()
         }
     }
+}
+
+fun Modifier.dashedBorder(
+    color: Color,
+    shape: Shape,
+    strokeWidth: Dp = 2.dp,
+    dashLength: Dp = 4.dp,
+    gapLength: Dp = 4.dp,
+    cap: StrokeCap = StrokeCap.Round
+) = dashedBorder(brush = SolidColor(color), shape, strokeWidth, dashLength, gapLength, cap)
+
+
+fun Modifier.dashedBorder(
+    brush: Brush,
+    shape: Shape,
+    strokeWidth: Dp = 2.dp,
+    dashLength: Dp = 4.dp,
+    gapLength: Dp = 4.dp,
+    cap: StrokeCap = StrokeCap.Round
+) = this.drawWithContent {
+
+    val outline = shape.createOutline(size, layoutDirection, density = this)
+
+    val dashedStroke = Stroke(
+        cap = cap,
+        width = strokeWidth.toPx(),
+        pathEffect = PathEffect.dashPathEffect(
+            intervals = floatArrayOf(dashLength.toPx(), gapLength.toPx())
+        )
+    )
+
+    drawContent()
+
+    drawOutline(
+        outline = outline,
+        style = dashedStroke,
+        brush = brush
+    )
 }
