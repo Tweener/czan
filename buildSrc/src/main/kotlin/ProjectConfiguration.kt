5edf7b4b8723ab62d08796b677e292bf5e9f09b5
Vivien Mahé <vivouneuh@gmail.com>
2026-03-05 10:21:49 +0100

    Bump Czan to version 2.11.2

--- buildSrc/src/main/kotlin/ProjectConfiguration.kt
+++ buildSrc/src/main/kotlin/ProjectConfiguration.kt
@@ -9,7 +9,7 @@ object ProjectConfiguration {
 
     object Czan {
         const val packageName = "com.tweener.czan"
-        const val versionName = "2.11.1"
+        const val versionName = "2.11.2"
         const val namespace = "$packageName.android"
         const val compileSDK = 36
         const val minSDK = 24
